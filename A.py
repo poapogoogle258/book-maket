d152385036b4f0628b28b028ead30f323d9761a2
E2rPhiphatphong <48234153+E2rPhiphatphong@users.noreply.github.com>
2019-03-25 12:34:09 +0700

    Update A.py

--- A.py
+++ A.py
@@ -1,62 +1,47 @@
-def HOME():
-    print("1-คณิตศาสตร์")
-    print("2-ภาษาไทย")
-    print("3-วิทยาศาตร์")
-    selet=int(input("***กรุณาเลือกหมวดหนังสือ***\n"))
-    if selet==1:
-        print(">1.1-แคลคลูลัส    "+"ราคา"+"เล่มละ250"+"บาท")
-        print(">1.2-สถิติ        "+"ราคา"+"เล่มละ180"+"บาท")
-        print(">1.3-สมการ      "+"ราคา"+"เล่มละ125"+"บาท")
-        print("กลับสู่เมนูหลักกดเลข "+"0")
-        a=int(input())
-        if a in (0,1,2,3):
-            if a==0:
-                HOME()
-        else :
-            print("คุณทำรายการไม่ถูกต้อง // กรุณาทำรายการใหม่")
-            eletmath=input("Please your mamber")
-    elif selet==2:
-        print(">2.1-วรรณคดี      "+"ราคา"+"เล่มละ125"+"บาท")
-        print(">2.2-ลำนำ        "+"ราคา"+"เล่มละ125"+"บาท")
-        print(">2.3-หลักภาษา     "+"ราคา"+"เล่มละ125"+"บาท")
-        print("กลับสู่เมนูหลักกดเลข "+"0")
-        a=int(input())
-        if a in (0,1,2,3):
-            if a==0:
-                HOME()
-        else :
-            print("คุณทำรายการไม่ถูกต้อง // กรุณาทำรายการใหม่")
-            seletthai=input("Please your mamber")
-    elif selet==3:
-        print(">3.1-ชีววิทยา       "+"ราคา"+"เล่มละ125"+"บาท")
-        print(">3.2-ฟืสิกส์        "+"ราคา"+"เล่มละ125"+"บาท")
-        print(">3.3-เคมี         "+"ราคา"+"เล่มละ125"+"บาท")
-        print("กลับสู่เมนูหลักกดเลข "+"0")
-        a=int(input())
-        if a in (0,1,2,3):
-            if a==0:
-                HOME()
-        else :
-            print("คุณทำรายการไม่ถูกต้อง // กรุณาทำรายการใหม่")
-            seletsci=input("Please your mamber")
-    else :
-        print("คุณทำรายการไม่ถูกต้อง // กรุณาทำรายการใหม่")
+x = int(input('Number of subjects'))
+a=[]
+for i in range(1,x+1):
+    print('subjects',i)
+    a1=input('subject (1-7 charcters) :')
+    a2=float(input('credit (0.00-12.00) (cradits:)'))
+    print("A,Excellent,4.0")
+    print("B+,Good,3.5")
+    print("B,Good,3.0")
+    print("C+,Fair,2.5")
+    print("C,Fair,2.0")
+    print("D+,Poor,1.5")
+    print("D,Poor,1.0")
+    print("F,Fall,0.0")
+    aa=input()
+    if aa=='A'or aa=='a':
+        aa=4.0
+    elif aa=='B+'or aa=='b+':
+        aa=3.5
+    elif aa=='B' or aa=='b':
+        aa=3.0
+    elif aa=='C+'or aa=='c+':
+         aa=2.5
+    elif aa=='C'or aa=='c':
+        aa=2.0
+    elif aa=='D+'or aa=='d+':
+        aa=1.5
+    elif aa=='D'or aa=='d':
+        aa=1.0
+    elif aa=='F'or aa=='f':
+        aa=0
+    a.append([a1,a2,aa])
     
-    
-    
-username = "e2r"
-password = "123"
-while(True):
-    use = input('Please enter your USERNAME:')
-    if use == username :
-        pwd = input('Please enter your PASSWORD:')
-        if pwd == password:
-            print("Hello, "+username+"")
-            HOME()
-            
-            break
-    print("กรุณาใส่หรัสใหม่อีกครัง\n")
-
-    
+print("summary Data")
+print("Number Subject Credit Grade")
+total1=0
+total2=0
+for i in range(x):
+    print(a[i][0],a[i][1],a[i][2])
+    total1=total1+a[i][1]*a[i][2]
+    total2=total2+a[i][1]
+   
+print("GPA = %.2f" %(total1/total2))
 
 
+        
+          
